3c335f9eac886ba7b4b012ed366eff2151da27af
edsilberberg <eric.silberberg@gmail.com>
2017-05-26 21:56:52 -0400

    Current files,
    workingwith 2 sensors, 45 second loop

---
+++ featherrx-receivetemp-union-bmp280/featherrx-receivetemp-union-bmp280.ino
@@ -0,0 +1,262 @@
+//note conert packetnum from float back to int
+//note problem in the loop with receive failed, also with the else logic action performed
+
+// Feather9x_RX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (receiver)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_TX
+#include <AdafruitIO.h>
+#include <AdafruitIO_Dashboard.h>
+#include <AdafruitIO_Data.h>
+#include <AdafruitIO_Definitions.h>
+#include <AdafruitIO_Feed.h>
+#include <AdafruitIO_Group.h>
+#include <AdafruitIO_MQTT.h>
+#include <AdafruitIO_WiFi.h>
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#include "config.h"
+
+// Feather m0 w/wing 
+#define RFM95_RST     11   // "A"
+#define RFM95_CS      10   // "B"
+#define RFM95_INT     6    // "D"
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+// set up the adadfruitIO feeds
+AdafruitIO_Feed *temperature = io.feed("OfficeTemp");
+AdafruitIO_Feed *voltage = io.feed("OfficeBattery");
+AdafruitIO_Feed *packet = io.feed("packetnum");
+AdafruitIO_Feed *Pressure = io.feed("OfficePressure");
+AdafruitIO_Feed *Altitude = io.feed("OfficeAltitude");
+
+typedef union
+{
+  float floatingPoint;                                                          // this union allows floting point numbers to be sent as binary representations over the wireless link
+  byte binary[sizeof(floatingPoint)];                                           // "sizeof" function takes care of determining the number of bytes needed for the floating point number
+} binaryFloat;
+
+binaryFloat airTemp;                                                            // ***************************************************************************
+binaryFloat counter;                                                            // remote stations can send up to five floating point values as defined here
+binaryFloat pressure;                                                           // Outside stations will typically send all five values
+binaryFloat altitude;                                                           // Inside stations will typically send airTemp and humidity values
+binaryFloat batVolt;   
+
+float dataF;
+float battery;
+float packetnum;
+float dataP; //pressure from bmp280
+float dataA; //altitude from bmp280
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+
+  Serial.begin(9600);
+  delay(100);
+  Serial.println("network status");
+
+ // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+  
+Serial.println("Feather LoRa Receiver");
+   
+  // manual reset
+  digitalWrite(LED, HIGH);
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+  digitalWrite(LED, LOW);
+  Serial.println("Feather LoRa Reset complete");
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+} // close setup loop
+
+void loop()
+{
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+   
+  if (rf95.available()) //true if a new, complete, error-free uncollected message is available to be retreived by recv()
+   {
+  Serial.println();
+  Serial.println("New data available to process");
+  getRadioPacket();   
+      /*
+     // Send a reply
+      uint8_t data[] = "Data received";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED, LOW);
+*/
+      // save the current temp to the analog feed
+  Serial.print("sending temp to adafruit.io -> ");
+  Serial.println(dataF);
+  temperature->save(dataF);
+
+      // save the current voltage to the analog feed
+  Serial.print("sending battery to adafruit.io-> ");
+  Serial.println(battery);
+  voltage->save(battery);
+
+    // save the current packetnumber to the analog feed
+  Serial.print("sending packetnum to adafruit.io-> ");
+  Serial.println(packetnum);
+  packet->save(packetnum);
+
+      // save the current pressure to the analog feed
+  Serial.print("sending pressure to adafruit.io-> ");
+  Serial.println(dataP);
+  Pressure->save(dataP);
+
+    // save the current altitude to the analog feed
+  Serial.print("sending altitude to adafruit.io-> ");
+  Serial.println(dataA);
+  Altitude->save(dataA);
+
+    Serial.println("Seconds until next packet...");
+    for (int i = 45; i >= 1; --i) {
+    Serial.print("#"); Serial.println(i, DEC);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    }
+   }
+  else 
+  {
+  Serial.print("Last packet recieved was packetnum "); Serial.println(packetnum);
+  Serial.println("No new data received. Waiting 10 seconds for new packet");
+      for (int i = 10; i >= 1; --i) {
+    Serial.print("#"); Serial.println(i, DEC);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+      }
+  }
+} // close void loop
+
+boolean getRadioPacket(void) /* read a packet from the LoRa Radio */
+{
+  byte buf[RH_RF95_MAX_MESSAGE_LEN];                                            // create a buffer for the RFM95W packet
+  byte len = sizeof(buf);                                                       // number of bytes in the buffer
+  if (rf95.recv(buf, &len))                                                     // Turns the receiver on if it not already on. If there is a valid message available, copy it to buf and return true else return false.
+  {
+   for (int n = 0; n < 4; n++)                                                 // read the bytes from the buffer into union struct for binary - float byte mapping
+    {
+      airTemp.binary[n] = buf[n];
+     // gndTemp.binary[n] = buf[n + 4];
+     // humidity.binary[n] = buf[n + 8];
+      batVolt.binary[n] = buf[n+4];
+      counter.binary[n] = buf[n+6];
+      pressure.binary[n] = buf[n+10];
+      altitude.binary[n] = buf[n+14];
+      
+    }
+  //  stationNum = buf[20];                                                       // retrieve the ID of the station sending the transmission
+/*
+    if (fahrenheit)                                                             // convert temperatures to fahrenheit if selected
+    {
+      gndTemp.floatingPoint = gndTemp.floatingPoint * 9 / 5 + 32;
+      airTemp.floatingPoint = airTemp.floatingPoint * 9 / 5 + 32;
+    }
+    pressure.floatingPoint = (pressure.floatingPoint / 3386.39) + pressCal;     // convert Pascal to inches of mercury; comment out for Pascal
+
+ 
+ */
+    dataF = airTemp.floatingPoint;                 // move all data into the corresponding station data structure
+  //  station[stationNum].data[GND_TEMP] = gndTemp.floatingPoint;
+  //  station[stationNum].data[HUMI] = humidity.floatingPoint;
+ //   station[stationNum].data[PRES] = pressure.floatingPoint;
+    battery = batVolt.floatingPoint;
+    packetnum = counter.floatingPoint;
+    dataA = altitude.floatingPoint;
+    dataP = pressure.floatingPoint;
+
+    digitalWrite(LED, HIGH);
+    delay(300);
+    digitalWrite(LED, LOW);
+    delay(300);
+    
+    digitalWrite(LED, HIGH);
+    delay(300);
+    digitalWrite(LED, LOW);
+    delay(300);
+      
+      
+      RH_RF95::printBuffer("Received buffer data of: ", buf, len);
+      Serial.print("Signal RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+      delay(10);
+      Serial.print("Packet number ");
+      Serial.print(counter.floatingPoint);
+      Serial.println(" contains: ");
+      Serial.print("batVolt.floatingPoint = ");
+      Serial.println(batVolt.floatingPoint);
+      Serial.print("airTemp.floatingPoint = ");
+      Serial.println(airTemp.floatingPoint);      
+      Serial.print("pressure.floatingPoint = ");
+      Serial.println(pressure.floatingPoint);
+      Serial.print("altitude.floatingPoint = ");
+      Serial.println(altitude.floatingPoint);      
+                 
+      return true;
+  }
+  else return false;
+}
+
---
+++ feathertx-temp-sleep-union-bmp280/feathertx-temp-sleep-union-bmp280.ino
@@ -0,0 +1,336 @@
+
+/* /modeled after adafruit feathertx,rx code at https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts/rfm9x-test
+and lora project at https://github.com/RodNewHampshire/LoRa-IOT-Home-Environment-Monitoring-System
+and mikenz at https://github.com/mikenz/Feather_M0_LoRa
+
+sensor1 ds18b20 Three wires - Orange Stripe connects to 3-5V, White connects to ground and Blue Stripe is data. https://www.adafruit.com/product/642
+sensor2 bmp280  https://learn.adafruit.com/adafruit-bmp280-barometric-pressure-plus-temperature-sensor-breakout/wiring-and-test
+
+version 1.0 functioning 5/17/17
+version 1.1 added sleep and packet counter using rtczero 5/22
+version 1.2 added bmp 280. Moved ds1820b from pin 20 to a2, did't work on pin 6 (gpio analog)
+
+
+features to add
+added in 1.1 pcket counter for transmit
+added in 1.1 sleep mode loop
+added in 1.2 temp and pressure data
+voltage divider for charge state
+battery vs solar status
+possible RTC to power device on and off.
+LED blink on transmit
+Led blink on boot and initialize
+
+to fix:
+change packet counter to int, 
+get a better understanding of the union/struct and add int variable within instead of after
+
+*/
+
+#define SENSOR_BMP280           // BMP280
+#define LED 13
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_Sensor.h>
+#include <RH_RF95.h>
+#include <RTCZero.h>
+
+
+boolean matched;
+int packetnum = 0;  // packet counter, we increment per xmission
+float dataC; //tempC from ds1820B
+float dataF; //tempC from ds1820B
+float dataP; //pressure from bmp280
+float dataA; //altitude from bmp280
+float battery; //battery voltage from vbat7
+byte wirelessPacket[18];                                // defines the size of the packet to be sent by the Remote Stations: 5 floats x 4 bytes per float +
+
+
+typedef union
+{
+  float floatingPoint;                                  // this union allows floting point numbers to be sent as binary representations over the wireless link
+  byte binary[sizeof(floatingPoint)];                   // "sizeof" function takes care of determining the number of bytes needed for the floating point number
+} joinedData;
+
+joinedData airTemp;                                    // ***************************************************************************
+joinedData counter;                                    // remote stations can send up to five floating point values as defined here
+joinedData altitude;                                   // Outside stations will typically send all five values
+joinedData pressure;                                   // Inside stations will typically send airTemp and humidity values
+joinedData batVolt;                                    // ***************************************************************************
+
+
+
+
+//add bmp280 as i2c
+Adafruit_BMP280 bme; // I2C
+
+// Data wire is plugged into port A2 on the Arduino
+#define ONE_WIRE_BUS A2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Feather m0 w/wing 
+#define RFM95_RST     11   // "A"
+#define RFM95_CS      10   // "B"
+#define RFM95_INT     6    // "D"
+
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+//settings for sleep timer
+RTCZero rtc;
+int AlarmTime;
+//const int alarminterval = 30;
+
+void setup() 
+{
+  rtc.begin(); 
+  pinMode(LED, OUTPUT);   
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+   
+   //while (!Serial);
+  Serial.begin(57600);
+  delay(100);
+  
+  //setup bmp280
+  Serial.println(F("BMP280 test"));
+  
+  if (!bme.begin()) {  
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+  
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(LED, HIGH);
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+  digitalWrite(LED, LOW);
+  Serial.println("Feather LoRa Reset complete");
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  readSensors();
+  delay(1000);
+  readSensors();
+  buildWirelessPacket();                                // build a wireless packet
+  sendWirelessPacket();    
+}
+
+//main loop
+void loop()
+{
+  if (matched) {
+    matched = false;
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
+    Serial.println("I'm back...");
+    readSensors();
+    buildWirelessPacket();
+    sendWirelessPacket();
+  }
+  AlarmTime = rtc.getSeconds()+45; // Adds 30 seconds to alarm time
+  AlarmTime = AlarmTime % 60; // checks for roll over 60 seconds and corrects
+  Serial.print("Next Alarm Time:");
+  Serial.println(AlarmTime);
+  rtc.setAlarmSeconds(AlarmTime); // Wakes at next alarm time
+  rtc.enableAlarm(rtc.MATCH_SS); // Match seconds only
+  rtc.attachInterrupt(alarmMatch);
+  
+  //Serial.end();
+  //USBDevice.detach(); // Safely detach the USB prior to sleeping
+  
+ rtc.standbyMode();    // Sleep until next alarm match
+
+ //USBDevice.attach();   // Re-attach the USB, audible sound on windows machines
+
+  // Simple indication of being awake
+  
+   // Serial.begin(9600);
+  //while (! Serial); // Wait until Serial is ready
+ // Serial.println("Awake");  
+  
+ /*
+ digitalWrite(LED, HIGH);
+ delay(500);
+ digitalWrite(LED, LOW);
+// replyWirelessPacket();
+
+*/
+
+//delay(30000);
+}
+
+//functions are below
+
+float BatteryStats()
+{
+// non loop version of battery voltage function
+#define VBATPIN A7
+float measuredvbat = analogRead(VBATPIN);
+measuredvbat *= 2;    // we divided by 2, so multiply back
+measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+measuredvbat /= 1024; // convert to voltage
+return measuredvbat;
+}
+
+
+void alarmMatch() // Do something when interrupt called
+{
+  matched = true;
+  }
+
+void readSensors(void)  /* reads all sensor values */
+{         
+ //get temperature
+  sensors.requestTemperatures();
+  dataC = sensors.getTempCByIndex(0);
+  Serial.print("The current temperature in my office is ");
+  Serial.print(dataC);
+  Serial.print(" degrees C, and  ");
+  dataF = (DallasTemperature::toFahrenheit(dataC)); // Converts tempC to Fahrenheit
+  Serial.print(dataF);
+  Serial.println(" degrees F."); 
+  airTemp.floatingPoint = dataF;
+  
+   dataP = bme.readPressure();
+   Serial.print("Pressure = ");
+   Serial.print(bme.readPressure());
+   Serial.println(" Pa");
+   pressure.floatingPoint = dataP;
+
+   dataA = (bme.readAltitude(1001.35));
+   Serial.print("Approx altitude = ");
+   Serial.print(dataA);
+   Serial.println(" m");
+   altitude.floatingPoint = dataA;
+
+  
+  // Get battery status
+battery = BatteryStats();
+Serial.print("Battery voltage is ");
+Serial.print(battery);
+Serial.println(" volts.");
+batVolt.floatingPoint = battery;
+
+Serial.print("new variable batVolt.floatingPoint contains ");
+Serial.println(batVolt.floatingPoint);
+
+Serial.print("new variable airTemp.floatingPoint contains ");
+Serial.println(airTemp.floatingPoint);
+
+Serial.print("packet counter is ");
+Serial.println(packetnum);
+counter.floatingPoint = (float)packetnum;
+
+Serial.print("new variable pressure.floatingPoint contains ");
+Serial.println(pressure.floatingPoint);
+
+Serial.print("new variable altitude.floatingPoint contains ");
+Serial.println(altitude.floatingPoint);
+}
+
+void buildWirelessPacket(void)  /* builds wireless packet to be transmitted by LoRa Radio */
+{                                                       
+  for(int n=0; n<4; n++)                                // read binary values from joinedData union struct into wireless packet
+  {
+    wirelessPacket[n] = airTemp.binary[n];
+    wirelessPacket[n+4] = batVolt.binary[n];
+    wirelessPacket[n+6] = counter.binary[n];
+    wirelessPacket[n+10] = pressure.binary[n];
+    wirelessPacket[n+14] = altitude.binary[n];
+  }
+
+int length=sizeof(wirelessPacket);  
+Serial.print("building packet containing ");
+Serial.print(length);
+Serial.println ("bytes");   
+
+//  wirelessPacket[20] = stationID;                       // add Remote Station ID to wireless packet
+  }
+
+  void sendWirelessPacket() /* wakes up LoRa Radio and transmits wireless packet */
+{
+  packetnum++;
+  rf95.send(wirelessPacket, sizeof(wirelessPacket));  
+  Serial.println("sending packet");
+  delay(10);
+  rf95.waitPacketSent();
+  rf95.sleep();                                         // put LoRa Radio into sleep mode
+
+}
+void replyWirelessPacket() //see reply from rx node
+{
+  
+  // Now wait for a reply
+  uint8_t indatabuf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(indatabuf);
+
+  Serial.println("Waiting for reply..."); delay(10);
+
+
+  if (rf95.waitAvailableTimeout(3000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(indatabuf, &len))
+   {
+      // Serial print "got reply:" and the reply message from the server
+      Serial.print("got reply: ");
+      Serial.println((char*)indatabuf);
+          Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+   }
+     else
+     {
+      //
+      Serial.println("recv failed");
+     }
+  }
+  else
+  {
+    // Serial print "No reply, is rf95_server running?" if don't get the reply .
+    Serial.println("No reply, is rf95_server running?");
+  }
+  delay(400);
+
+  }
+
